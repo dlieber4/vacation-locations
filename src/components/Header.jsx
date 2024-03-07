b3e248e01e6f72f00210409bf1d2e689ed3879b9
Dov Lieber <Dovi344@yahoo.com>
2024-03-07 16:37:04 -0500

    14 commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,10 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header-info">
+        <div>
+          <input type="text" placeholder="Search..." />
+        </div>
+
         <h3>By Dovi Lieber</h3>
         <h2>
           If you are looking for a place to go on vacation then you came to the
