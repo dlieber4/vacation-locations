9bab644b5377f4e5d49ef0b47aa416cf0b89b36c
Dov Lieber <Dovi344@yahoo.com>
2024-03-07 15:34:25 -0500

    9th commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,55 @@
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer-info">
-        <h1>More information is available below</h1>
-        <h3>Hotels</h3>
-        <h4>North America Hotels</h4>
-        <h4>Central America Hotels</h4>
-        <h4>South America Hotels</h4> <h4>Europe Hotels</h4>
-        <h4>Asia Hotels</h4>
-        <h4>Australia & New Zealand Hotels</h4>
-        <h3>Transportation</h3>
-        <h4>Bus Times & Prices</h4> <h4>Train Times & Prices</h4>{' '}
-        <h4>Taxi Information</h4>
-        <h3>Tours & Attractions</h3>
-        <h4>North America Tours</h4>
-        <h4>Central America Tours</h4>
-        <h4>South America Tours</h4> <h4>Europe Tours</h4>
-        <h4>Asia Tours</h4>
-        <h4>Australia & New Zealand Tours</h4>
-        <h2>
-          Thank you all for visiting Vacationlocations.com! I hope you all you
+    <div>
+      <div className="flex-container">
+        <div className="flex-item">More information is available</div>
+        <div className="flex-item">Hotels</div>
+        <div className="flex-item">North America Hotels</div>
+        <div className="flex-item">Central America Hotels</div>
+        <div className="flex-item">South America Hotels</div>
+        <div className="flex-item">Europe Hotels</div>
+        <div className="flex-item">Asia Hotels</div>
+        <div className="flex-item">Australia & New Zealand Hotels</div>
+        <div className="flex-item">Transportation</div>
+        <div className="flex-item">Bus Times & Prices</div>
+        <div className="flex-item">Train Times & Prices</div>
+        <div className="flex-item">Taxi Information</div>
+        <div className="flex-item">Tours & Attractions</div>
+        <div className="flex-item">North America Tours</div>
+        <div className="flex-item">Central America Tours</div>
+        <div className="flex-item">South America Tours</div>
+        <div className="flex-item">Europe Tours</div>
+        <div className="flex-item">Asia Tours</div>
+        <div className="flex-item">Australia & New Zealand Tours</div>
+        <div className="flex-item">
+          Thank you all for visiting Vacationlocations.com! I hope you all
           enjoyed and you are now able to plan your dream vacation.
-        </h2>
+        </div>
+      </div>
+      <div className="grid-container">
+        <div className="grid-item">More information is available</div>
+        <div className="grid-item">Hotels</div>
+        <div className="grid-item">North America Hotels</div>
+        <div className="grid-item">Central America Hotels</div>
+        <div className="grid-item">South America Hotels</div>
+        <div className="grid-item">Europe Hotels</div>
+        <div className="grid-item">Asia Hotels</div>
+        <div className="grid-item">Australia & New Zealand Hotels</div>
+        <div className="grid-item">Transportation</div>
+        <div className="grid-item">Bus Times & Prices</div>
+        <div className="grid-item">Train Times & Prices</div>
+        <div className="grid-item">Taxi Information</div>
+        <div className="grid-item">Tours & Attractions</div>
+        <div className="grid-item">North America Tours</div>
+        <div className="grid-item">Central America Tours</div>
+        <div className="grid-item">South America Tours</div>
+        <div className="grid-item">Europe Tours</div>
+        <div className="grid-item">Asia Tours</div>
+        <div className="grid-item">Australia & New Zealand Tours</div>
+        <div className="grid-item">
+          Thank you all for visiting Vacationlocations.com! I hope you all
+          enjoyed and you are now able to plan your dream vacation.
+        </div>
       </div>
     </div>
   )
